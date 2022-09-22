46e0d0f69ffae54681403410e92ad9d8bedad4cb
anthero <anthero@clownworld.rocks>
2022-09-22 17:35:40 -0500

    reduced alphabet

--- allyourbase.c
+++ allyourbase.c
@@ -12,12 +12,17 @@
 
 void show_help(void);
 
+
 int main(int argc, char *argv[]) {
 
 	long base, num; 
 	long place = 1;
 	int  count = 0;
-	char base_alphabet[64];
+	char base_alphabet[] = {
+		'0', '1', '2', '3', '4', '5', '6', '7',
+		'8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+		'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'o', 'p', 'q', 'r', 's', 't', 'u', 'v' };
 
 	//prevents segfaults, displays help
 	if (argc != 3) {
@@ -37,28 +42,6 @@ int main(int argc, char *argv[]) {
 		
 		//start of actual program
 		else {
-			if (base < 32) {
-				char temp[] = { 
-					'0', '1', '2', '3', '4', '5', '6', '7',
-					'8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-					'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-					'o', 'p', 'q', 'r', 's', 't', 'u', 'v' };
-				for (int i = 0; i < 32; i++)
-					base_alphabet[i] = temp[i];
-
-			} else {
-				char temp[] = {
-					'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
-					'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-					'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
-					'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
-					'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
-					'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-					'w', 'x', 'y', 'z', '0', '1', '2', '3', 
-					'4', '5', '6', '7', '8', '9', '+', '/' };
-				for (int i = 0; i < 64; i++)
-					base_alphabet[i] = temp[i];
-			}
 
 			//qol stuff for output
 			if      (base == 2)
